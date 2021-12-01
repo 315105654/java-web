3292f6304fa39d42714ff5a9f87262567d6f7dba
drew.liu <315105654@qq.com>
2021-12-01 13:06:32 +0800

    OK

---
+++ servlet-app/src/main/java/OneTest.java
@@ -0,0 +1,12 @@
+/*import org.junit.Test;*/
+
+public class OneTest {
+
+    /*@Test*/
+    public void test_1(){
+        System.out.println(123);
+    }
+}
+
+
+
---
+++ servlet-app/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,50 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// 处理登录的请求的
+// 配置访问链接
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * 处理 GET 请求
+     * @param req 请求
+     * @param resp 响应
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("处理 GET 请求");
+        // 获取请求中【携带的参数】
+        String userName = req.getParameter("user_name");
+        String passWord = req.getParameter("pass_word");
+
+        System.out.println("账号：" + userName);
+        System.out.println("密码：" + passWord);
+    }
+
+    /**
+     * 处理 POST 请求
+     * @param req 请求
+     * @param resp 响应
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("处理 POST 请求");
+        // 获取请求中【携带的参数】
+        String userName = req.getParameter("user_name");
+        String passWord = req.getParameter("pass_word");
+
+        System.out.println("账号：" + userName);
+        System.out.println("密码：" + passWord);
+    }
+}
